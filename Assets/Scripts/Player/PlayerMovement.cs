a875eeec3e2f306600b2eba879efce4a79d610cb
AkeemKhan <akeemkhan@outlook.com>
2018-07-04 20:34:29 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject Player;
+    public PlayerController PlayerController;
+    public Vector3 ClickPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ClickPosition = Player.transform.position;   	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SetMoveToPoint();
+        Movement();
+    }
+
+    public void SetMoveToPoint()
+    {
+        float move = Input.GetAxis("Fire1");
+        if (move > 0)
+        {
+            ClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GetRotationPosition();
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                ClickPosition.z = transform.position.z;
+            }
+
+        }
+    }
+
+    public void Movement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, ClickPosition, PlayerStatistics.Speed * Time.deltaTime);
+    }
+
+    public void GetRotationPosition()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStatistics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatistics
+{
+    // Player Stats
+    public static int MaxHealth;
+    public static int Health;
+    public static int Stamina;
+    public static float MaxSpeed;
+    public static float Speed;
+    public static float RotateSpeed;
+    public static float Movex;
+    public static float Movey;
+
+    // Game Stats
+    public static int FloorsCleared;
+    public static int RoomsFound;
+    public static int Detected;
+    public static int PrisonersRescued;
+    public static int SecretRoomsFound;
+    public static int KeyCardsFound;
+    public static int EnemiesKilled;
+}
