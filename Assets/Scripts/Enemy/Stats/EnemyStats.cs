cfd4dff78e8f289f4703aece6394f0de955cda10
AkeemKhan <akeemkhan@outlook.com>
2018-07-06 21:59:00 +0100

    Started on EnemyStats

---
+++ Assets/Scripts/Enemy/Stats/EnemyStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyStats
+{
+    public readonly float Speed;
+    public readonly float PersueSpeed;
+    public readonly float AlertSpeed;
+    public readonly float PatrolSpeed;
+
+    public readonly float FovAngleStrong;
+    public readonly float FovAngleWeak;
+    public readonly float DetectRange;
+    public readonly float RaycastOffset;
+    public readonly float PatrolRange;
+
+    public readonly int FireCount;
+    public readonly float FireCooldown;
+    public readonly float FireRate;
+
+    public readonly float AlertPhaseCountdown;
+    public readonly float AlertRate;
+    public readonly float AlertCounter;
+    public readonly float AlertRange;
+    public readonly float CamaraRotateRate;
+}
